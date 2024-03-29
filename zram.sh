a46163e80b3e6335841141f5d5faf315d2a4f590
mrdotx <klassiker@gmx.de>
2024-03-29 07:55:14 +0100

    performance optimizations and fixes

--- zram.sh
+++ zram.sh
@@ -3,19 +3,19 @@
 # path:   /home/klassiker/.local/share/repos/zram/zram.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/zram
-# date:   2024-03-28T08:26:28+0100
+# date:   2024-03-29T07:53:30+0100
 
 # speed up script by using standard c
 LC_ALL=C
 LANG=C
 
 # config
-num_devices="1"     # number of swaps
-algorithm="zstd"    # lzo-rle is used without specified algorithm
+num_devices=1       # number of swaps to create
+algorithm="zstd"    # the default algorithm is zstd (lzo,lzo-rle,lz4,lz4hc,842)
 zram_percent=       # without a specified percentage of ram,
                     # the stored value of the algorithm is used
-max_size=4096       # maximum size of the swaps in MiB (<1 use max calculated)
-swap_prio=42        # higher numbers indicate higher swap priority (0 - 32767)
+max_size=4096       # maximum size of the swaps in MiB (<=0 use max calculated)
+priority=42         # higher numbers indicate higher swap priority (0 - 32767)
 
 # functions
 check_root() {
@@ -24,9 +24,9 @@ check_root() {
         && exit 1
 }
 
-set_value() {
+set_sysfs() {
     [ -e "$1" ] \
-        && printf "Set %s to %s\n" "$1" "$2" \
+        && printf "Set %s = %s\n" "$1" "$2" \
         && printf "%s" "$2" > "$1"
 }
 
@@ -34,25 +34,25 @@ enable_optimizations() {
     # optimizing swap on zram (https://wiki.archlinux.org/title/Zram)
     case $1 in
         1)  # optimized values
-            set_value "/sys/module/zswap/parameters/enabled" 0
-            set_value "/proc/sys/vm/swappiness" 180
-            set_value "/proc/sys/vm/watermark_boost_factor" 0
-            set_value "/proc/sys/vm/watermark_scale_factor" 125
-            set_value "/proc/sys/vm/page-cluster" 0
+            set_sysfs '/sys/module/zswap/parameters/enabled' N
+            set_sysfs '/proc/sys/vm/swappiness' 180
+            set_sysfs '/proc/sys/vm/watermark_boost_factor' 0
+            set_sysfs '/proc/sys/vm/watermark_scale_factor' 125
+            set_sysfs '/proc/sys/vm/page-cluster' 0
             ;;
         0)  # default values
-            set_value "/proc/sys/vm/page-cluster" 3
-            set_value "/proc/sys/vm/watermark_scale_factor" 10
-            set_value "/proc/sys/vm/watermark_boost_factor" 15000
-            set_value "/proc/sys/vm/swappiness" 60
-            set_value "/sys/module/zswap/parameters/enabled" 1
+            set_sysfs '/proc/sys/vm/page-cluster' 3
+            set_sysfs '/proc/sys/vm/watermark_scale_factor' 10
+            set_sysfs '/proc/sys/vm/watermark_boost_factor' 15000
+            set_sysfs '/proc/sys/vm/swappiness' 60
+            set_sysfs '/sys/module/zswap/parameters/enabled' Y
             ;;
     esac
 }
 
 activate_devices() {
     # enable optimizations
-    [ "$1" = "optimized" ] \
+    [ "$1" = 'optimized' ] \
         && enable_optimizations 1
 
     # add zram to kernel modules
@@ -61,11 +61,11 @@ activate_devices() {
     # calculate zram size
     [ -z "$zram_percent" ] \
         && case $algorithm in
-            zstd)   zram_percent="73";;
-            842)    zram_percent="48";;
-            lz4hc)  zram_percent="66";;
-            lz4)    zram_percent="62";;
-            *)      zram_percent="63";;
+            zstd)   zram_percent='73';;
+            842)    zram_percent='48';;
+            lz4hc)  zram_percent='66';;
+            lz4)    zram_percent='62';;
+            *)      zram_percent='63';;
         esac
 
     memory=$(free -b | awk 'NR==2 {print $2}')
@@ -75,36 +75,29 @@ activate_devices() {
         && max_size=$((max_size * 1024 * 1024 / num_devices)) \
         && size=$((max_size > size ? size : max_size))
 
-    # activate zram
-    for i in $(seq "$num_devices"); do
-        device=$((i - 1))
-
-        [ -n "$algorithm" ] \
-            && algorithm="--algorithm $algorithm"
-        eval "zramctl $algorithm --size $size /dev/zram$device"
-
-        mkswap --uuid clear --label "zram$device" "/dev/zram$device"
-        swapon --priority $swap_prio "/dev/zram$device"
+    # activate zram swaps
+    for device in $(seq 0 "$((num_devices - 1))"); do
+        set_sysfs "/sys/block/zram$device/comp_algorithm" "$algorithm" \
+            && set_sysfs "/sys/block/zram$device/disksize" "$size" \
+            && mkswap --quiet --uuid clear --label "zram$device" \
+                "/dev/zram$device" \
+            && swapon --priority $priority "/dev/zram$device"
     done
-
-    unset i
 }
 
 deactivate_devices() {
     # deactivate zram
-    devices=$(awk 'NR>1 {print $1}' /proc/swaps) \
-        && for i in $devices; do
-            swapoff "$i"
+    awk 'NR>1 {print $1}' /proc/swaps \
+        | while read -r swaps; do
+            swapoff "$swaps"
         done
 
     # remove zram from kernel modules
     modprobe --remove zram
 
     # disable optimizations
-    [ "$1" = "optimized" ] \
+    [ "$1" = 'optimized' ] \
         && enable_optimizations 0
-
-    unset i
 }
 
 # options
